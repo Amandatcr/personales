71fd84741c4549d827404fd1076e8fe85ec3e287
Amanda Cruz <tacr95@educa.madrid.org>
2026-03-06 11:24:43 +0100

    Configurar deploy Astro GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://amandatcr.github.io',
+  base: '/Curriculum-astro/',
   vite: {
     plugins: [tailwindcss()]
   }
